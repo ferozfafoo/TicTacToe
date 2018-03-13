3a33e579800164fcef68ec90b9b585bc34862b2f
Feroz Fafoo <30866441+ferozfafoo@users.noreply.github.com>
2018-03-13 15:20:42 +0100

    Add files via upload

---
+++ TicTacToe/TicTacToe/MarkType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The type of value cell in the game is current at 
+/// </summary>
+namespace TicTacToe
+{
+	public enum MarkType
+	{
+		/// <summary>
+		/// The cell hasnt been clicked yet 
+		/// </summary>
+		Free,
+
+		/// <summary>
+		/// The cell is O
+		/// </summary>
+		Nought,
+
+		/// <summary>
+		/// The cell is X
+		/// </summary>
+		Cross
+
+	}
+}
